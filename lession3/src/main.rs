fb7fe1fab9ec34556a8fed05b66842635ad7e79c
lao king <wangjian5748@gmail.com>
2022-05-04 18:58:23 +0800

    update video

--- lession3/src/main.rs
+++ lession3/src/main.rs
@@ -1,41 +1,43 @@
-use std ::net::{TcpListener,TcpStream};
-use std::thread;
-use std::time;
-use std::io;
-use std::io::{Read,Write};
- 
- 
-fn handle_client(mut stream: TcpStream) -> io::Result<()>{
- 
-    let mut buf = [0;512];
-    for _ in 0..1000{
-        let bytes_read = stream.read(&mut buf)?;
-        if bytes_read == 0{
-            return Ok(());
+use std::io::{Read, Result, Write};
+use std::net::{SocketAddr, TcpListener, TcpStream};
+
+pub fn main() -> std::io::Result<()> {
+    //在本地8080端口创建TCP连接，将字符串解析为一个 SocketAddr 类型，如果解析失败，抛出异常
+    let addr = "127.0.0.1:8080".parse::<SocketAddr>().unwrap();
+    let listener = TcpListener::bind(addr)?;
+    //不断的接收连接进来，等同于循环的 listener.accept()
+    for stream in listener.incoming() {
+        //模式匹配，incoming得到的是一个Result,有可能是Ok，也有可能是Err。
+        match stream {
+            Ok(stream) => {
+                handle_client(stream)
+                    .unwrap_or_else(|error| eprintln!("Server error got: {:?}", error));
+                //如果遇到错误，会打印错误，错误由 stream的read，write 发出
+            }
+            Err(e) => eprintln!("onnection failed!"), //连接错误
         }
- 
-        stream.write(&buf[..bytes_read])?;
-        thread::sleep(time::Duration::from_secs(1));
- 
     }
     Ok(())
 }
- 
-fn main() -> io::Result<()>{
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let mut thread_vec: Vec<thread::JoinHandle<()>> = Vec::new();
-    
-    for stream in listener.incoming() {
-        let stream = stream.expect("failed");
- 
-        let handle = thread::spawn(move || {
-            handle_client(stream).unwrap_or_else(|error| eprintln!("{:?}",error))
-        });
-        thread_vec.push(handle);
-    }
- 
-    for handle in thread_vec {
-        handle.join().unwrap();
-    }
+
+fn handle_client(mut stream: TcpStream) -> Result<()> {
+    //创建一个缓冲区，用来保存数据
+    let mut buf = [0; 500];
+    //循环读取数据，如果读取数据异常直接返回
+    while match stream.read(&mut buf) {
+        Ok(n) if n == 0 => {
+            //如果读取到的⻓度是0，表示关闭连接，函数返回
+            false
+        }
+        Ok(n) => {
+            println!("received:{}", String::from_utf8_lossy(&buf[..n]));
+            //将读取到的内容返写到客户端去
+            stream.write(&buf[..n])?;
+            true
+        }
+        Err(e) => {
+            false //遇到错误，返回
+        }
+    } {}
     Ok(())
 }
